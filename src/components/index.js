bcb611e5136c02a1518178f9adb5a73aa2f13705
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2023-12-05 00:05:30 -0800

    done

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from './Buttons';
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedbackCard from "./FeedbackCard";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import Buttons from "./Buttons";
+import GetStarted from "./GetStarted";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clients,
+    CTA,
+    Stats,
+    Footer,
+    Testimonials,
+    Hero,
+    FeedbackCard,
+    Buttons,
+    GetStarted,
+};
