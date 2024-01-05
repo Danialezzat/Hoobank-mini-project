095f6c309adb81ae5f8dbc53a3b858fc1e43be14
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-05 02:51:13 -0800

    footer is fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,11 +33,11 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='w-full felx justify-between items-center md:flex-row flex-col pr-6 border-t-[1px] border-t-[#3F3E45] '>
-        <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+      <div className='w-full pt-6 flex justify-between items-center pr-6 border-t-[1px] border-t-[#3F3E45] '>
+        <p className='font-poppins  font-normal text-center text-[18px] leading-[27px] text-white'>
           2023 HooBank. All Rights Reserved.
         </p>
-        <div className='flex flex-row md:mt-0 mt-6'>
+        <div className='flex justify-center items-center flex-row  '>
           {
             socialMedia.map((social, index) => (
               <img key={social.id} src={social.icon} alt={social.id} className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}/>
