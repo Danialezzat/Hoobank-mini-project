254b2bc29eb61359ebafc542d6f3616d1cdb4163
Danialezzat <146076454+Danialezzat@users.noreply.github.com>
2024-01-06 12:54:29 -0800

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => {
   return (
-    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col `}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
         <div className='flex-1 flex flex-col justify-start mr-10'>
           <img src={logo} alt="hoobank" className='w-[266px] h-[32px] object-contain' />
@@ -15,7 +15,7 @@ const Footer = () => {
           </p>
         </div>
 
-        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wram md:mt-0 mt-10'>
+        <div className='flex-[1.5] w-full flex flex-row justify-between ic flex-wram md:mt-0 mt-10'>
           {
             footerLinks.map(footerLink => (
               <div key={footerLink.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
@@ -33,7 +33,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='w-full pt-6 flex justify-between items-center pr-6 border-t-[1px] border-t-[#3F3E45] '>
+      <div className='w-full sm:flex-row flex-col   pt-6 flex justify-between items-center pr-6 border-t-[1px] border-t-[#3F3E45] '>
         <p className='font-poppins  font-normal text-center text-[18px] leading-[27px] text-white'>
           2023 HooBank. All Rights Reserved.
         </p>
